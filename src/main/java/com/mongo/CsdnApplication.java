ed9589427bbf9c9dc3d9ca4d52147062d7c77a25
林泽锋 <282245889@qq.com>
2018-04-13 17:29:02 +0800

    增加maven依赖以及增加mongodb的数据库连接池和spring security放行目录的配置

--- src/main/java/com/mongo/CsdnApplication.java
+++ src/main/java/com/mongo/CsdnApplication.java
@@ -1,9 +1,11 @@
 package com.mongo;
 
+import com.spring4all.mongodb.EnableMongoPlus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableMongoPlus
 public class CsdnApplication {
 
 	public static void main(String[] args) {
