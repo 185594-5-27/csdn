a78135c126b38aeefcfe051b6bd4a8a7d46f819c
林泽锋 <282245889@qq.com>
2018-04-13 15:59:00 +0800

    Initial commit

---
+++ src/main/java/com/mongo/CsdnApplication.java
@@ -0,0 +1,12 @@
+package com.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsdnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsdnApplication.class, args);
+	}
+}
